9a6fd19e7d38ac6713deeec17a3e33c4fc9e92c6
Juan <juanssal@gmail.com>
2020-09-24 11:08:58 +0200

    changed some routes to the myApp.js file

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use(express.static(__dirname + "/public"));
-app.use("/public", express.static(__dirname + "/public"));
 
 
 // app.use('/public', express.static(process.cwd() + '/public'));
@@ -58,10 +56,6 @@ app.use("/public", express.static(__dirname + "/public"));
 //       .send(err.message || 'SERVER ERROR');
 //   }
 // })
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
 
 
 
